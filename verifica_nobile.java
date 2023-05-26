f1b5bc0685197ecbae019156d8764585e04e5214
orango tango <125025861+Federico-Natale@users.noreply.github.com>
2023-05-26 12:26:21 +0200

    Update verifica_nobile.java

--- verifica_nobile.java
+++ verifica_nobile.java
@@ -1,102 +1,97 @@
 package nobile;
-import javax.swing.*;
-import javax.swing.event.*;
-import javax.awt.*;
-import javax.awt.event.*;
-import javax.awt.event.actionListener;
-import java.awt.BorderLayout;
-import java.awt.Component;
+
+import java.awt.*;
+import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Collections;
 
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.SwingUtilities;
+import javax.swing.*;
 
+public class verifica_nobile {
+    private ArrayList<Integer> numberList;
+    private DefaultListModel<Integer> listModel;
+    private JList<Integer> numberListView;
+    private JTextField inputField;
 
+    public verifica_nobile() {
+        numberList = new ArrayList<>();
+        listModel = new DefaultListModel<>();
+        numberListView = new JList<>(listModel);
 
-	public class verifica_nobile {
+        inputField = new JTextField();
+        inputField.setPreferredSize(new Dimension (20, 30));
+        JButton addButton = new JButton("Aggiungi");
+        JButton showButton = new JButton("Visualizza");
 
-	    private ArrayList<Integer> numberList;
-	    private DefaultListModel<Integer> listModel;
-	    private Component numberListView;
-	    private JTextField inputField;
+        addButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                addNumberToList(inputField.getText());
+            }
+        });
 
-	    public verifica_nobile() {
-	    	
-	        numberList = new ArrayList<>();
-	        listModel = new DefaultListModel<>();
+        showButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                showNumbers();
+            }
+        });
 
-	        inputField = new JTextField();
-	        JButton addButton = new JButton("Aggiungi");
-	        JButton showButton = new JButton("Visualizza");
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new FlowLayout());
+        inputPanel.add(inputField);
+        inputPanel.add(addButton);
+        inputPanel.add(showButton);
+     
 
-	        addButton.addActionListener(new ActionListener() {
-	            public void actionPerformed(ActionEvent e) {
-	                addNumberToList(inputField.getText());
-	            }
-	        });
+        JFrame frame = new JFrame("ArrayList");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.add(inputPanel, BorderLayout.NORTH);
+        frame.add(new JScrollPane(numberListView), BorderLayout.CENTER);
+        frame.setSize(500, 600);
+        frame.setVisible(true);
+    }
 
-	        showButton.addActionListener(new ActionListener() {
-	            public void actionPerformed(ActionEvent e) {
-	                showNumbers();
-	            }
-	        });
+    private void addNumberToList(String numberString) {
+        try {
+            int number = Integer.parseInt(numberString);
+            int index = 0;
 
-	        JPanel inputPanel = new JPanel();
-	        inputPanel.setLayout(new FlowLayout());
-	        inputPanel.add(inputField);
-	        inputPanel.add(addButton);
-	        inputPanel.add(showButton);
+            for (int i = 0; i < numberList.size(); i++) {
+                if (numberList.get(i) <= number) {
+                    index = i + 1;
+                } else {
+                    break;
+                }
+            }
 
-	        JFrame frame = new JFrame("ArrayList");
-	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.setLayout(new BorderLayout());
-	        frame.add( numberListView, BorderLayout.NORTH);
-	        frame.add(new JScrollPane(numberListView), BorderLayout.CENTER);
-	        frame.setSize(300, 200);
-	        frame.setVisible(true);
-	    }
+            numberList.add(index, number);
+            listModel.addElement(number);
+        } catch (NumberFormatException e) {
+            System.out.println("Inserisci un numero intero valido.");
+        }
+    }
 
-	    
-	    private void addNumberToList(String numberString) {
-	        try {
-	            int number = Integer.parseInt(numberString);
+    private void showNumbers() {
+        Collections.sort(numberList);
+        listModel.clear();
+        for (Integer number : numberList) {
+            listModel.addElement(number);
+        }
+    }
 
-	            
-	            int index = Collections.binarySearch(numberList, number);
-	            if (index < 0) {
-	                index = -(index + 1);
-	            }
-
-	            numberList.add(index, number);
-	            listModel.addElement(number);
-	        } catch (NumberFormatException e) {
-	            System.out.println("Inserisci un numero intero valido.");
-	        }
-	    }
-
-	    private void showNumbers() {
-	        Collections.sort(numberList);
-	        listModel.clear();
-	        for (Integer number : numberList) {
-	            listModel.addElement(number);
-	        }
-	    }
-
-	    public static void main(String[] args) {
-	        SwingUtilities.invokeLater(new Runnable() {
-	            @Override
-	            public void run() {
-	                new verifica_nobile();
-	            }
-	        });
-	    }
-	
-
-
-
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new verifica_nobile();
+            }
+        });
+    }
 }
+
+        // ho usato al posto del comando "Collections.binarySearch()" un "for"
+	// il main era già presente nel precedente programma
+	// ho aggiunto gli import mancanti, che mi ero dimenticato di aggiungere nella verifica
 	
 	/* al codice manca la classe main per essere eseguito, inoltre sono poco sicuro del fatto che sia farina del tuo sacco.
 	 * Ad esempio sai dirmi cosa fà il metodo "Collections.binarySearch()" ?.
